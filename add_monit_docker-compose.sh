77d526ad03dbbd747affe485efac26c16b851ad4
Louis Poncet <8927060+needsone@users.noreply.github.com>
2020-12-09 14:39:26 +0100

    - Configuration of monit to check the docker container status

---
+++ add_monit_docker-compose.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if [ ! -f docker-compose.yml ] then
+  echo "We are not in a docker-compose repository"
+  exit
+fi
+
+docker-compose ps -a | cut -d ' ' -f 1| tail -n +3 > /tmp/list_containers.txt
+
+if [ ! -d "/opt/monit/bin" ] ; then
+  mkdir -p /opt/monit/bin
+fi
+
+for i in `cat /tmp/list_containers.txt` ; do
+echo "#!/bin/sh
+
+sudo docker top ${i};
+exit \$?;
+" > /opt/monitrc/bin/docker_check_${i}.sh
+
+chmod +x  /opt/monitrc/bin/docker_check_${i}.sh
+
+echo "
+check program ${i} with path /opt/monit/bin/docker_check_${i}.sh
+  if status != 0 then alert
+" > /etc/monit/conf.d/docker_${i}
+done
+
+rm /tmp/list_containers.txt
---
+++ add_monit_docker.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ "X$1" = "X" ]
+then
+echo "I need container name"
+exit
+fi
+
+if [ ! -d "/opt/monit/bin" ]
+then
+  mkdir -p /opt/monit/bin
+fi
+
+echo "#!/bin/sh
+
+sudo docker top ${container};
+exit \$?;
+" > /opt/monit/bin/docker_check_${container}.sh
+
+chmod +x  /opt/monit/bin/docker_check_${container}.sh
+
+echo "
+check program ${container} with path /opt/monit/bin/docker_check_${container}.sh
+  if status != 0 then alert
+" > /etc/monit/conf.d/docker_${container}
